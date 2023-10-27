bae2f51506152e44e8f023c5307c35a71385d126
Nitish kumar <112773560+nistha01@users.noreply.github.com>
2023-10-27 12:33:01 +0530

    Time: 2 ms (97.37%), Space: 47 MB (13.12%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,32 +1,36 @@
-public class Solution {
+class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
-        
-        for (int num : nums) {
-            if (candidate1 == num) count1++;
-            else if (candidate2 == num) count2++;
-            else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
+        // if(nums.length==1 || nums.length==2)return 
+        int num1=0;
+        int num2=0;
+        int c1=0;
+        int c2=0;
+        List<Integer> list=new ArrayList<>();
+        for(int ele:nums){
+            if(ele==num1)c1++;
+            else if(ele==num2)c2++;
+            else if(c1==0){
+                num1=ele;
+                c1=1;
+            }
+            else if(c2==0){
+                num2=ele;
+                c2=1;
+            }
+            else{
+                c1--;
+                c2--;
             }
         }
-        
-        count1 = count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
+        c1=0;
+        c2=0;
+        for(int num:nums){
+            if(num==num1)c1++;
+            else if(num==num2)c2++;
         }
-        
-        List<Integer> ans = new ArrayList<>();
-        if (count1 > nums.length / 3) ans.add(candidate1);
-        if (count2 > nums.length / 3) ans.add(candidate2);
-        
-        return ans;
+        if(c1>nums.length/3)list.add(num1);
+        if(c2>nums.length/3)list.add(num2);
+        return list;
+            
     }
 }
