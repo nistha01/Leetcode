b60190cee541ef1cdd11d3f4e2329ffc5b8ed89d
Nitish kumar <112773560+nistha01@users.noreply.github.com>
2023-12-08 00:14:50 +0530

    Time: 1 ms (99.00%), Space: 59.1 MB (48.29%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -2,19 +2,20 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
       int i = 0;
         while(i < nums.length) {
-            int correct = nums[i] - 1;
-            if(nums[i] <= nums.length && nums[i] > 0 && nums[i] != nums[correct]) {
+            //for any number nums[i] ..correct position is nuber-1 ,like for 4 its position is 4-1=3
+            int correctPosition = nums[i] - 1;
+            if(nums[i] <= nums.length && nums[i] > 0 && nums[i] != nums[correctPosition]) {
 
                 int temp = nums[i];
-                nums[i] = nums[correct];
-                nums[correct] = temp;
+                nums[i] = nums[correctPosition];
+                nums[correctPosition] = temp;
             }
             else {
                 i++;
             }
         }
 
-        int count = 0;
+ 
         for(int j = 0; j < nums.length; j++) {
 
             if(nums[j] != j + 1) {
